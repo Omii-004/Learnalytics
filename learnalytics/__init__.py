bfef728a2362cddfeed793a2f513e5f03e0614ba
Joey-1123 <shubhampanchal9168@gmail.com>
2026-06-05 19:43:27 +0530

    Add developer credits footer to all main templates + __init__.py

---
+++ learnalytics/__init__.py
@@ -0,0 +1,7 @@
+CREDITS = {
+    "developers": [
+        "Shubham Panchal (https://github.com/Joey-1123)",
+        "Omkar Tamalwad (https://github.com/Omii-004)",
+    ],
+    "project": "Learnalytics",
+}
